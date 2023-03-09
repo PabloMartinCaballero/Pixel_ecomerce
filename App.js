4e337f56e09cac861fc216d8a723e84ac06f3efd
“Pablo <Pablo_martin_caballero21@hotmail.comgit config user.emailgir user.namegit user.namegit config --global user.name Pablo>
2023-03-09 17:19:22 -0300

    pixel

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './component/navbar/navbar';
+import Cart from './component/cartwidget/cart';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
