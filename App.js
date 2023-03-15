70a045aac70fe0ee652809f70b0bdf543e3a55a9
“Pablo <Pablo_martin_caballero21@hotmail.comgit config user.emailgir user.namegit user.namegit config --global user.name Pablo>
2023-03-15 20:44:33 -0300

    item

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/navbar/navbar';
 import Cart from './component/cartwidget/cart';
+import ItemListContainer from './component/item/item';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Cart />
+      <ItemListContainer title = {"Carrito de compras"} />
     </div>
   );
 }
